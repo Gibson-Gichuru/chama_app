8234b8aa0fbce9395342f49b6d907195be87dd4f
Gibson-Gichuru <gichuru.developer@gmail.com>
2022-07-24 11:55:27 +0300

    The update compares the current migration Id on the database and  the Migration ID found on the latest  script head If there is a difference then. run the migrations
    The script only needs to connect to the database  atlest once to check the migration ID.

--- api/deploy.sh
+++ api/deploy.sh
@@ -1,24 +1,45 @@
-#!/bin/sh
+#!/bin/bash
 
-function run_production_server(){
+function deploy_container {
 
-    gunicorn --bind 0.0.0.0:5000 manage:app
-    
-}
+    ENV="production"
 
-function run_migrations(){
+    IFS=' '
 
-    if [ $FLASK_ENV != "development" ];then
+    if [ "$FLASK_ENV" = "$ENV" ];then
 
-        flask deploy
+        # GETTING THE CURRENT DB MIGRATION REVISION
 
-        if [ $? -eq 0 ]; then run_production_server; fi
+        DB_REVISION=`flask db current`
+
+        read -a REVISION_ARRAY <<< "$DB_REVISION"
+
+        # GET THE CURRENT HEAD ON THE SCRIPT DIRECTORY
+
+        SCRIPT_CURRENT_HEAD=`flask db heads`
+
+        # CHECK IF THE MIGRATION REVISION ID AND SCRIPT HEAD ID MATCH
+
+        if [ "${REVISION_ARRAY[0]}" != "${SCRIPT_CURRENT_HEAD[0]}" ];then
+
+            # PERFORM migration update
+
+            `flask deploy`
+
+        fi
+
+        # The Production server should be lauched either way
+
+        `gunicorn --bind 0.0.0.0:5000 manage:app`
+
+    else
+
+        echo "::Current Application not set for Production"
 
     fi
 
 
+
 }
 
-run_migrations
-
-if [ $? -gt 0 ]; then echo "Unable to run database migrations";fi
+deploy_container
