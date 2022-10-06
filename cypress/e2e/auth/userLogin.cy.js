7514f936a6df6630f9fe8e9991a48a90634b6b6b
Gibson-Gichuru <gichuru.developer@gmail.com>
2022-10-06 16:30:20 +0300

    Input Field Tests
    form input fields are now tested by e2e tests instead of Component testing
    
    Reason:
    
        The application will have different input fields each with its own configuration
        thus a Component test is  not suited for such a case because we will need to write
        a test for each component
    
        Tackling such on a e2e test actually simulates a user interaction with the component.

--- cypress/e2e/auth/userLogin.cy.js
+++ cypress/e2e/auth/userLogin.cy.js
@@ -7,13 +7,34 @@ describe("User should be able to login in", ()=>{
 
 
         // test that input errors out on Blur
+        formFieldsShouldErrorOutOnUserBlur()
 
+        // test  that input error out on invalid user input
 
-        cy.get('[data-testid=testUserEmailField]')
-        .blur()
-
-        cy.contains("Required!")
-
+        formFieldsShouldErrorOutOnInvalidUserInput()
     })    
 
 })
+
+function formFieldsShouldErrorOutOnInvalidUserInput(){
+
+    // get the email field and type  on it
+
+    cy.get('#email').type("some cool staff")
+
+    // get the password field and type on it
+
+    cy.get('#password').type("some pass").blur()
+
+    cy.contains("Invalid Email")
+    cy.contains("password should contain atleast one number and one special character")
+
+    // assert that the fields have errors
+}
+
+function formFieldsShouldErrorOutOnUserBlur(){
+    cy.get('#email').focus().blur()
+    cy.get('#password').focus().blur()
+
+    cy.contains("required!")
+}
