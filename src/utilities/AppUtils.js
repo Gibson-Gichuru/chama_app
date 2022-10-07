7f614d1ec7c16c336507fffca3896dc76ac731c9
Gibson-Gichuru <gichuru.developer@gmail.com>
2022-10-07 15:48:46 +0300

    clean up unused code

--- src/utilities/AppUtils.js
+++ src/utilities/AppUtils.js
@@ -1,32 +1,18 @@
 import dayjs from "dayjs";
 import jwtDecode from "jwt-decode";
 
-import {TextField} from "@mui/material";
-
 export function checkIsExpired(token){
 
     try{
         let  token_info = jwtDecode(token)
+
         if(dayjs.unix(token_info.exp).diff(dayjs())<1){
             return true
         }
     }catch(error){
+
         return true
     }
 
     return false
-}
-
-export function generateFormFields(fields){
-
-    let formFields = {}
-
-    fields.map((field)=>{
-
-        let formField = <TextField {...field.common}/>
-
-        formFields[field.name]= formField
-    })
-
-    return formFields
 }
