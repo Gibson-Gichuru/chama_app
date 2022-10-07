5bf3a0acbde5ef27185c84d3677b3a7e388a28b4
Gibson-Gichuru <gichuru.developer@gmail.com>
2022-10-07 15:56:59 +0300

    registered dialog reducer to the root reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,11 +3,13 @@ import {combineReducers} from "redux"
 import messageReducer from "./Message/MessageReducer";
 import alertReducer from "./Alert/AlertReducer";
 import userReducer from "./User/UserReducer";
+import dialogReducer from "./Dialog/DialogReducer";
 
 const rootReducer = combineReducers({
     alerts:alertReducer,
     messages:messageReducer,
     user:userReducer,
+    dialog:dialogReducer,
 })
 
 
